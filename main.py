fa9ee7448bfbb7f980c0c2b4343fa6483ded4883
Soappression <justinpoplin@proton.me>
2025-06-07 21:43:54 -0400

    added def character_count to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(filepath):
 
@@ -13,9 +13,10 @@ def main():
     file_content = get_book_text(filepath)
     
     words = word_count(file_content)
-    
+    characters = character_count(file_content)
     num_words = len(words)
     print(f"{num_words} words found in the document")
+    print(characters)
 
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,15 @@ def word_count(file_content):
     
 
     return words
+
+def character_count(file_content):
+    char_count = {}
+    file_content = file_content.lower()
+    
+    for character in file_content:
+        if character in char_count:
+            char_count[character] += 1
+        else:
+            char_count[character] = 1
+
+    return char_count
