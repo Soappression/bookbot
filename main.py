f618a03ebc363355ff64113265a50f7eb42082df
Soappression <justinpoplin@proton.me>
2025-06-07 22:59:08 -0400

    no longer hard coded / finished bookbot

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-from stats import word_count, character_count
+import sys
+from stats import word_count, character_count, sorter
+
+def main():
+    
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    file_content = get_book_text(sys.argv[1])
+    
+    words = word_count(file_content)
+    characters = character_count(file_content)
+    sorted_instances = sorter(characters)
+    num_words = len(words)
+    print(f"Found {num_words} total words")
+    
+    for item in sorted_instances[0:2]:
+        print(f"{item['char']}: {item['num']}")
+
 
 def get_book_text(filepath):
 
     with open(filepath) as f:
         file_content = f.read()
-
     return file_content
-
-def main():
-    
-    filepath = "books/frankenstein.txt"
-    file_content = get_book_text(filepath)
-    
-    words = word_count(file_content)
-    characters = character_count(file_content)
-    num_words = len(words)
-    print(f"{num_words} words found in the document")
-    print(characters)
-
 
 main()
--- stats.py
+++ stats.py
@@ -10,9 +10,24 @@ def character_count(file_content):
     file_content = file_content.lower()
     
     for character in file_content:
-        if character in char_count:
-            char_count[character] += 1
-        else:
-            char_count[character] = 1
+        if character.isalpha():
+            if character in char_count:
+                char_count[character] += 1
+            else:
+                char_count[character] = 1
 
     return char_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorter(char_count):
+    instances = []
+    for char in char_count:
+        num = char_count[char]
+        instances.append({"char": char, "num": num})
+    
+
+
+    instances.sort(reverse=True, key=sort_on)
+    return instances
