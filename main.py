cbda5c0dfb50d1bc28752003b77190ad24bc5d93
Soappression <justinpoplin@proton.me>
2025-06-07 23:23:01 -0400

    Added comments to main.py and stats.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from stats import word_count, character_count, sorter
 
 def main():
     
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2: # says if length of sys.argv is not 2 throw this error
         print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
+        sys.exit(1) # This exits the program with a non-zero status and throws an error
     
-    file_content = get_book_text(sys.argv[1])
+    file_content = get_book_text(sys.argv[1]) # sets the filepath parameter to sys.argv[1] which is just index 1 in sys.argv (index 0 is main.py)
     
     words = word_count(file_content)
     characters = character_count(file_content)
@@ -15,14 +15,14 @@ def main():
     num_words = len(words)
     print(f"Found {num_words} total words")
     
-    for item in sorted_instances[0:2]:
-        print(f"{item['char']}: {item['num']}")
+    for item in sorted_instances[0:2]: # loops over first 2 items in sorted_instances
+        print(f"{item['char']}: {item['num']}") # prints key 'char' and value 'num'
 
 
 def get_book_text(filepath):
 
-    with open(filepath) as f:
-        file_content = f.read()
+    with open(filepath) as f: # with will stop when finished
+        file_content = f.read() # sets everything inside to a string
     return file_content
 
 main()
--- stats.py
+++ stats.py
@@ -1,33 +1,33 @@
 def word_count(file_content):
     
-    words = file_content.split()
+    words = file_content.split() # splits the text into a list of words () empty () means it splits text based on whitespace
     
 
     return words
 
 def character_count(file_content):
     char_count = {}
-    file_content = file_content.lower()
+    file_content = file_content.lower() # makes all file_content lowercase
     
     for character in file_content:
-        if character.isalpha():
-            if character in char_count:
-                char_count[character] += 1
-            else:
-                char_count[character] = 1
+        if character.isalpha(): # if character is letter...
+            if character in char_count: # if character is in dictionary...
+                char_count[character] += 1 # add 1
+            else: # otherwise
+                char_count[character] = 1 # set character as key with value 1 / loop restarts after this, character has now been added to dictionary
 
     return char_count
 
-def sort_on(dict):
-    return dict["num"]
+def sort_on(dict): # creates key sort_on
+    return dict["num"] # returns the value "num" from a dictionary
 
 def sorter(char_count):
-    instances = []
+    instances = [] # empty list
     for char in char_count:
-        num = char_count[char]
-        instances.append({"char": char, "num": num})
+        num = char_count[char] # iterating over char_cound {}
+        instances.append({"char": char, "num": num}) # adding dictionary with key "char" and vlaue "num" to instances list
     
 
 
-    instances.sort(reverse=True, key=sort_on)
+    instances.sort(reverse=True, key=sort_on) # sort list in decending order with the key sort_on
     return instances
